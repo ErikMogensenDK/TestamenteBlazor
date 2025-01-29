da81665739429e51aee82c7fcc816cedbdae53b0
ErikMogensenDK <erikmogensen1@gmail.com>
2025-01-29 10:15:24 +0100

    added table tracking people added and spouse-question component

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,10 +1,11 @@
-namespace Models
+namespace FluentTestamente.Models
 {
 	public class Person
 	{
-	public string Name { get; set; }
-	public string Address { get; set; }
-	public DateTime BirthDate { get; set; }
-	public bool IsAlive { get; set; }
-}
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public DateTime? BirthDate { get; set; }
+		public bool IsAlive { get; set; } = true;
+		public string DateOnlyBirthDateString { get; set; }
+	}
 }
